6f25cdc6f549bcec61d72973ca0883f98b2dec32
EugeneSpiridonov <eugenespint@gmail.com>
2023-12-23 10:48:47 +0800

    add tests

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+from typing import Literal
 from pydantic_settings import BaseSettings
 
 
@@ -7,6 +8,11 @@ class Settings(BaseSettings):
     DB_USER: str
     DB_PASS: int
     DB_NAME: str
+    TEST_DB_HOST: str
+    TEST_DB_PORT: int
+    TEST_DB_USER: str
+    TEST_DB_PASS: int
+    TEST_DB_NAME: str
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
@@ -16,6 +22,7 @@ class Settings(BaseSettings):
     SMTP_PASS: str
     REDIS_HOST: str
     REDIS_PORT: int
+    MODE: Literal["DEV", "TEST", "PROD"]
 
     class Config:
         env_file = ".env"
